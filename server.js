88355736ad3464fd5e8d822f49cdddb70dc35d79
kawtar <kawtarlhbib293@gmail.com>
2026-05-20 13:06:12 +0200

    change route

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.get("/khanjar", (req, res) => {
   res.json({ message: "hello khanjar" });
 });
 
+app.get("/kawtar", (req, res) => {
+  res.json({ message: "i'm kawtar " });
+});
+
 app.listen(3000, () => {
   console.log("server is running");
 });
-
-
