db2dd4e006cb4d1ac339215d299a3f529be5b266
ibrahim <ibrahimzamane001@gmail.com>
2026-05-20 11:41:10 +0100

    change fiel name

--- ibrahim.js
+++
@@ -1,5 +0,0 @@
-console.log("hi irahim java script");
-
-document.querySelector("#btn").addEventListener("clik", () => {
-  console.log("hello brahim");
-});
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hi irahim java script");
+
+document.querySelector("#btn").addEventListener("click", () => {
+  console.log("hello ibrahim");
+});
