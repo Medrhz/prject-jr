92e90d8771f1b60c2925b3768bb78946979368d8
RHZIZA Mohamed <109476153+Medrhz@users.noreply.github.com>
2026-05-20 12:00:10 +0100

    add / route in server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 app = express();
 
+app.get("/", (req, res) => {
+  res.json({ message: "hello " });
+});
+
 app.listen(3000, () => {
   console.log("server is running");
 });
