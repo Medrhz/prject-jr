170a77f2eaee3f9a74ebdbd6882e4489d1e8ec89
ibrahim <ibrahimzamane001@gmail.com>
2026-05-20 12:02:01 +0100

    add ibrahim & khanjar

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
   res.json({ message: "hello " });
 });
 
+app.get("/ibrahim", (req, res) => {
+  res.json({ message: "hello ibrahim" });
+});
+
+app.get("/khanjar", (req, res) => {
+  res.json({ message: "hello khanjar" });
+});
+
 app.listen(3000, () => {
   console.log("server is running");
 });
+
+
